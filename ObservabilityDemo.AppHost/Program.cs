f74374925a167fa01148dff5145f112bd63249fd
Carl Sargunar <carl@sargunar.com>
2025-03-06 00:23:31 +0000

    Add Umbraco

--- ObservabilityDemo.AppHost/Program.cs
+++ ObservabilityDemo.AppHost/Program.cs
@@ -2,4 +2,8 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var weatherApi = builder.AddProject<Projects.WeatherAPI>("weatherapi");
 
+var website = builder.AddProject<Projects.UmbObservability>("website")
+    .WithReference(weatherApi)
+    .WaitFor(weatherApi);
+    
 builder.Build().Run();
